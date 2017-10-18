45e4175e972d35eb8b16f2370e327140b51d1dfa
Benjamin <ben@codafication.com>
2017-10-18 16:59:05 +1000

    Added same fix than text for textArea

--- src/TextArea.js
+++ src/TextArea.js
@@ -1,33 +1,32 @@
 /**
  * Created by steve on 15/09/15.
  */
-import React from 'react';
-import ComposedComponent from './ComposedComponent';
-import TextField from 'material-ui/TextField';
+import React from 'react'
+import ComposedComponent from './ComposedComponent'
+import TextField from 'material-ui/TextField'
 
 class TextArea extends React.Component {
-
-    render() {
-        // FIXME: Obviously fix rowsMax eventually..
-        //console.log('TextArea', this.props.form);
-        return (
-            <div className={this.props.form.htmlClass}>
-                <TextField
-                    type={this.props.form.type}
-                    floatingLabelText={this.props.form.title}
-                    hintText={this.props.form.placeholder}
-                    onChange={this.props.onChangeValidate}
-                    errorText={this.props.error}
-                    defaultValue={this.props.value}
-                    multiLine={true}
-                    rows={this.props.form.rows}
-                    rowsMax={this.props.form.rowsMax}
-                    disabled={this.props.form.readonly}
-                    style={this.props.form.style || {width: '100%'}}
-                />
-            </div>
-        );
-    }
+  render() {
+    // FIXME: Obviously fix rowsMax eventually..
+    //console.log('TextArea', this.props.form);
+    return (
+      <div className={this.props.form.htmlClass}>
+        <TextField
+          type={this.props.form.type}
+          floatingLabelText={this.props.form.title}
+          hintText={this.props.form.placeholder}
+          onChange={this.props.onChangeValidate}
+          errorText={this.props.error}
+          value={this.props.value}
+          multiLine={true}
+          rows={this.props.form.rows}
+          rowsMax={this.props.form.rowsMax}
+          disabled={this.props.form.readonly}
+          style={this.props.form.style || { width: '100%' }}
+        />
+      </div>
+    )
+  }
 }
 
-export default ComposedComponent(TextArea);
+export default ComposedComponent(TextArea)
