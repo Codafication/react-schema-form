387d40d9c0248c958bfbb070c95fe6f8d98fc459
Curtis Wilkinson <curtis@codafication.com>
2017-10-26 11:38:15 +1000

    Fixing the select box issue

--- src/Select.js
+++ src/Select.js
@@ -1,61 +1,70 @@
 /**
  * Created by steve on 15/09/15.
  */
-import React from 'react';
-import ComposedComponent from './ComposedComponent';
-import MenuItem from 'material-ui/MenuItem';
-import SelectField from 'material-ui/SelectField';
+import React from "react";
+import ComposedComponent from "./ComposedComponent";
+import MenuItem from "material-ui/MenuItem";
+import SelectField from "material-ui/SelectField";
 
 class Select extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onSelected = this.onSelected.bind(this);
 
-    constructor(props) {
-        super(props);
-        this.onSelected = this.onSelected.bind(this);
-        this.state = {
-            currentValue: this.props.model[this.props.form.key]
-            || (this.props.form.titleMap != null ? this.props.form.titleMap[0].value : '')
-        };
+    const initialValue =
+      this.props.model[this.props.form.key] ||
+      (this.props.form.titleMap != null
+        ? this.props.form.titleMap[0].value
+        : "");
+
+    this.state = {
+      currentValue: initialValue
+    };
+
+    const mockEvent = { target: { value: initialValue } };
+
+    this.props.onChangeValidate(mockEvent);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.model && nextProps.form.key) {
+      this.setState({
+        currentValue:
+          nextProps.model[nextProps.form.key] ||
+          (nextProps.form.titleMap != null
+            ? nextProps.form.titleMap[0].value
+            : "")
+      });
     }
+  }
 
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.model && nextProps.form.key) {
-            this.setState({
-                currentValue: nextProps.model[nextProps.form.key]
-                || (nextProps.form.titleMap != null ? nextProps.form.titleMap[0].value : '')
-            });
-        }
-    }
+  onSelected(event, selectedIndex, menuItem) {
+    this.setState({
+      currentValue: menuItem
+    });
+    event.target.value = menuItem;
+    this.props.onChangeValidate(event);
+  }
 
-    onSelected(event, selectedIndex, menuItem) {
+  render() {
+    const menuItems = this.props.form.titleMap.map((item, idx) => (
+      <MenuItem key={idx} primaryText={item.name} value={item.value} />
+    ));
 
-        this.setState({
-            currentValue: menuItem
-        });
-        event.target.value = menuItem;
-        this.props.onChangeValidate(event);
-    }
-
-    render() {
-        const menuItems = this.props.form.titleMap.map((item, idx) => (
-            <MenuItem key={idx}
-                      primaryText={item.name}
-                      value={item.value} />
-        ));
-
-        return (
-            <div className={this.props.form.htmlClass}>
-                <SelectField
-                    value={this.state.currentValue}
-                    floatingLabelText={this.props.form.title}
-                    disabled={this.props.form.readonly}
-                    onChange={this.onSelected}
-                    fullWidth={true} >
-
-                    {menuItems}
-                </SelectField>
-            </div>
-        );
-    }
+    return (
+      <div className={this.props.form.htmlClass}>
+        <SelectField
+          value={this.state.currentValue}
+          floatingLabelText={this.props.form.title}
+          disabled={this.props.form.readonly}
+          onChange={this.onSelected}
+          fullWidth={true}
+        >
+          {menuItems}
+        </SelectField>
+      </div>
+    );
+  }
 }
 
 // Select.propTypes = {
